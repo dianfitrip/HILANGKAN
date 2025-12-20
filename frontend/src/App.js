c8929b87524de5dd48da58694ae0a92848ca553a
dianfitrip <dianfitri683@gmail.com>
2025-12-20 22:50:54 +0700

    backend blm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProsedurPage from './pages/ProsedurPage';
+
 import FormPenemuanPage from './pages/FormPenemuanPage';
 import FormKehilanganPage from './pages/FormKehilanganPage';
-import ListBarangTemuanPage from './pages/ListBarangTemuanPage';
+
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/prosedur" element={<ProsedurPage />} />
+        <Route path="/list" element={<ListBarangTemuanPage />}/>
         <Route path="/lapor-penemuan" element={<FormPenemuanPage />} />
         <Route path="/lapor-kehilangan" element={<FormKehilanganPage />} />
-        <Route path="/list" element={<ListBarangTemuanPage />}/>
       </Routes>
     </Router>
   );
